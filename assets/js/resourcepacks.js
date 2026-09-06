7c6ac8491e50364b4f9b798d4ea23f7424bff8ac
sei0608 <seiicHi68116@outlook.jp>
2026-09-06 12:34:13 +0900

    Update resourcepacks.js

--- assets/js/resourcepacks.js
+++ assets/js/resourcepacks.js
@@ -54,7 +54,7 @@ function renderDetail(resourcepack) {
   resourcepack.versions.forEach(v => {
     const li = document.createElement("li");
     const a = document.createElement("a");
-    a.textContent = `${v.mc_version} - ${v.loader} ${v.resourcepack_version}`;
+    a.textContent = `${v.mc_version} - ${v.resourcepack_version}`;
     a.href = v.download_url;
     li.appendChild(a);
     ul.appendChild(li);
